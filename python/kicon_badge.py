1fdff644ad411657f2de372da2b10123137e5ab7
Maciej Suminski <orson@orson.net.pl>
2019-03-31 23:03:14 +0200

    Python: added underscores to private commands

--- python/kicon_badge.py
+++ python/kicon_badge.py
@@ -31,7 +31,7 @@ class KiconBadge:
         if self._serial:
             self._serial.close()
 
-    def crc(self, data):
+    def _crc(self, data):
         crc = 0
 
         try:
@@ -42,20 +42,20 @@ class KiconBadge:
 
         return crc
 
-    def make_resp(self, resp):
-        return struct.pack('>BBB', 1, resp, self.crc(resp))
+    def _make_resp(self, resp):
+        return struct.pack('>BBB', 1, resp, self._crc(resp))
 
-    def make_cmd(self, cmd, data=None):
+    def _make_cmd(self, cmd, data=None):
         if data:
             data_bytes = bytes('%c%s' % (cmd, data), 'ascii')
             data_len = len(data_bytes)
             return struct.pack('>B%dsB' % data_len,    # struct format
-                    data_len, data_bytes, self.crc(data_bytes))
+                    data_len, data_bytes, self._crc(data_bytes))
         else:
-            return struct.pack('>BBB', 1, cmd, self.crc(cmd))
+            return struct.pack('>BBB', 1, cmd, self._crc(cmd))
 
-    def check_resp(self):
-        expected_resp = self.make_resp(cmd_defs.CMD_RESP_OK)
+    def _check_resp(self):
+        expected_resp = self._make_resp(cmd_defs.CMD_RESP_OK)
         actual_resp = self._serial.read(len(expected_resp))
         if expected_resp != actual_resp:
             raise Exception('Error! Response: %s' % str(binascii.hexlify(actual_resp), 'ascii'))
@@ -63,7 +63,7 @@ class KiconBadge:
     def reset(self):
         # special command type, cannot be created with make_cmd
         reset_cmd = struct.pack('>B', cmd_defs.CMD_TYPE_RESET)
-        reset_resp = self.make_resp(cmd_defs.CMD_RESP_RESET)
+        reset_resp = self._make_resp(cmd_defs.CMD_RESP_RESET)
 
         self._serial.write(reset_cmd)
         time.sleep(0.1)
@@ -79,6 +79,6 @@ class KiconBadge:
                 raise Exception('No response after reset')
 
     def uart_send(self, data):
-        uart_cmd = self.make_cmd(cmd_defs.CMD_TYPE_UART, data)
+        uart_cmd = self._make_cmd(cmd_defs.CMD_TYPE_UART, data)
         self._serial.write(uart_cmd)
-        self.check_resp()
+        self._check_resp()

8df628492f859f820b1f801963fb0688ba4ae807
Maciej Suminski <orson@orson.net.pl>
2019-04-01 23:37:32 +0200

    Extended the Python test script

--- python/badge_test.py
+++ python/badge_test.py
@@ -18,12 +18,35 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 from kicon_badge import KiconBadge
+import time
 
 badge = KiconBadge('/dev/ttyACM0')
 badge.reset()
 uart_data = badge.uart_transfer(bytes('badge test', 'ascii'))
 
 badge.lcd_clear()
-badge.lcd_pixel(2, 2, 1)
-badge.lcd_text(1, 1, "aaa")
+
+# checker pattern
+for y in range(12):
+    for x in range(badge.LCD_WIDTH):
+        badge.lcd_pixel(x, y, (x ^ y) & 0x02)
+
+badge.lcd_text(1, 2, "hey, it is scripted")
+badge.lcd_text(4, 3, "with Python!")
 badge.lcd_refresh()
+
+badge.led_set(badge.LED1, 1)
+badge.led_blink(badge.LED2, 2)
+
+print("Buttons state: %x" % badge.buttons())
+
+# read the LCD status
+#badge.i2c_read(0x3c, 0x00, 1)
+# invert the LCD colors
+badge.i2c_write(0x3c, 0x00, bytes([0xA7]))
+time.sleep(1)
+# restore the LCD colors
+badge.i2c_write(0x3c, 0x00, bytes([0xA6]))
+
+badge.spi_config(1000, 0)
+print(badge.spi_transfer(bytes([0x01, 0x02, 0x03])))

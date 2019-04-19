c95088d3cee95abeb5a6c41f0bd4e675f8abcda7
Maciej Suminski <orson@orson.net.pl>
2019-04-19 15:12:29 +0200

    Added missing license headers

--- SSD1306_commands.h
+++ SSD1306_commands.h
@@ -1,8 +1,20 @@
 /*
- * SSD1306_registers.h
+ * Copyright (c) 2019 Katarzyna Stachyra <kas.stachyra@gmail.com>
  *
- *  Created on: Jan 20, 2019
- *      Author: kasik
+ * This source code is free software; you can redistribute it
+ * and/or modify it in source code form under the terms of the GNU
+ * General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #ifndef SSD1306_COMMANDS_H_
